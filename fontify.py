e07add337b99e0c2d856f21c49c1e0fe83f1a94e
JadingTsunami <JadingTsunami@users.noreply.github.com>
2024-08-19 18:12:56 -0700

    Add CWILV support.

--- fontify.py
+++ fontify.py
@@ -46,15 +46,36 @@ def text_to_image(
         img.im.paste(color, (0, 0) + mask_image.size, mask_image)
     return img
 
+def merge_images(img1, img2):
+    size1 = img1.size
+    size2 = img2.size
+
+    w = max(size1[0], size2[0])
+    woffset = int(round(abs(size1[0] - size2[0])/2, 0))
+    h = size1[1] + size2[1]
+
+    img = Image.new("RGBA", (w, h))
+
+    if w == size2[0]:
+        # img2 is bigger
+        img.paste(img1, (woffset,0))
+        img.paste(img2, (0,size1[1]))
+    else:
+        # img1 is bigger
+        img.paste(img1, (0,0))
+        img.paste(img2, (woffset,size1[1]))
+
+    return img
+
 # Main program
 data = None
 with open("fontify.json", "r") as j:
     data = json.load(j)
 
-def get(job, what, default=None):
+def get(job, what, default="no-default"):
     if what in job:
         return job[what]
-    if default:
+    if default != "no-default":
         return default
     raise TypeError(f"Error: {what} not provided and has no default.")
 
@@ -77,16 +98,39 @@ def process_job(job):
     shadowcolor = tuple(get(job, "shadowcolor", (int(color[0]/2), int(color[1]/2), int(color[2]/2))))
     outline = get(job, "outline", False)
     outlinecolor = tuple(get(job, "outlinecolor", (0,0,0)))
+    cwilv = get(job, "cwilv", False)
+    if not cwilv:
+        cwilv_color = get(job, "cwilv_color", False)
+        cwilv_lf = get(job, "cwilv_littlefont", False)
+        cwilv_lfs = get(job, "cwilv_littlefontsize", False)
+        cwilv_ol = get(job, "cwilv_outline", False)
+        cwilv_olc = get(job, "cwilv_outlinecolor", False)
+    else:
+        cwilv_color = tuple(get(job, "cwilv_color"))
+        cwilv_lf = get(job, "cwilv_littlefont")
+        cwilv_lfs = get(job, "cwilv_littlefontsize")
+        cwilv_ol = get(job, "cwilv_outline")
+        cwilv_olc = tuple(get(job, "cwilv_outlinecolor"))
 
     # execute the job
     strings = get(job, "strings", None)
 
     if strings:
-        for s in strings:
-            outfile = s
-            text = strings[s]
-            img = text_to_image(text, font, fontsize, color, fontmask, shadow, shadowcolor, outline, outlinecolor)
-            img.save(os.path.join(path, outfile + ".png"))
+        if cwilv:
+            for cw in strings:
+                outfile = cw
+                s0, s1 = strings[cw]
+                img1 = text_to_image(s0, font, fontsize, color, fontmask, shadow, shadowcolor, outline, outlinecolor)
+                img2 = text_to_image(s1, cwilv_lf, cwilv_lfs, cwilv_color, fontmask, False, False, cwilv_ol, cwilv_olc)
+
+                img = merge_images(img1, img2)
+                img.save(os.path.join(path, outfile + ".png"))
+        else:
+            for s in strings:
+                outfile = s
+                text = strings[s]
+                img = text_to_image(text, font, fontsize, color, fontmask, shadow, shadowcolor, outline, outlinecolor)
+                img.save(os.path.join(path, outfile + ".png"))
     else:
         # special case for STCFN*
         for n in range(33,96):
